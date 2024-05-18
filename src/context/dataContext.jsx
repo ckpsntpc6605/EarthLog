9b2620f5fa13ef54de6f36b293afc055be813f74
phil <ph3207ph3207@gmail.com>
2024-05-18 20:18:41 +0800

    Delete the state in context which is alternatived by zustand

--- src/context/dataContext.jsx
+++ src/context/dataContext.jsx
@@ -5,15 +5,10 @@ import useAuthListener from "../utils/hooks/useAuthListener";
 const DataContext = createContext();
 
 export const DataProvider = ({ children }) => {
-  // const [userCurrentClickedPost, setUserCurrentClickedPost] = useState(null);
-  // const [notSavedPoint, setNotSavedPoint] = useState(null);
   const currentUser = useAuthListener();
   const userPostData = useGetCurrentUserPosts();
   const mapRef = useRef();
   const [dayPlan, setDayPlan] = useState([{ day1: [] }]); //裡面每個項目都是一天[[day1],[day2]]
-  // const [currentDay, setCurrentDay] = useState(1);
-  // const [isModalOpen, setIsModalOpen] = useState(false);
-  // const [selectedPost, setSelectedPost] = useState(null);
   const [selectedUserData, setSelectedUserData] = useState({
     avatar: "",
     email: "",
@@ -23,23 +18,11 @@ export const DataProvider = ({ children }) => {
   return (
     <DataContext.Provider
       value={{
-        // userCurrentClickedPost,
-        // setUserCurrentClickedPost,
-        // notSavedPoint,
-        // setNotSavedPoint,
         userPostData,
         currentUser,
-        // destinationData,
-        // setDestinationData,
         mapRef,
         dayPlan,
         setDayPlan,
-        // currentDay,
-        // setCurrentDay,
-        // isModalOpen,
-        // setIsModalOpen,
-        // selectedPost,
-        // setSelectedPost,
         selectedUserData,
         setSelectedUserData,
       }}
